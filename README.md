681fcdd1b4ac3cfe630e3a53ead22ed508a2e4e9
EmotionBin <313200064@qq.com>
2022-02-08 20:26:58 +0800

    feat: 添加commitlint

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # vue2-cli
 一个基于 vue2 的脚手架
+
+## 未完成的部分，需要补充包
+### dependencies
+- vue-class-component
+- vue-property-decorator
+- vuex-module-decorators
+
+### devDependencies
+- @commitlint/config-conventional
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,25 @@
 // 这里可以对 git commit 进行一些规范
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // 开发新功能
+        'fix', // 修补 bug
+        'chore', // 日常事务
+        'refactor', // 重构
+        'perf', // 性能优化
+        'style', // 样式修改 不影响代码运行的变动
+        'build', // 构建项目
+        'test', // 添加测试
+        'revert', // 版本回滚
+        'docs', // 文档
+        'config', // 配置
+        'ci', // 持续集成
+        'test', // 测试
+      ]
+    ]
+  }
+}
